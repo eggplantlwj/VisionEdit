14513521257357ad50633290c1794b1340d06039
liu.wenjie <liu.wenjie>
2021-03-17 16:05:33 +0800

    1、优化程序架构，减少项目之间的引用，将所有工具类归并到一起，对工具和主函数之间进行解耦 2、增加Project概念，并对项目进行保存和载入。 3、优化LOG显示，修复一些其他的bug

---
+++ ToolLib.Log/LogForm/UserLogger.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace LogForm
+{
+    partial class UserLogger
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.ClearLog = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.listBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(365, 266);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ClearLog});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(107, 28);
+            // 
+            // ClearLog
+            // 
+            this.ClearLog.Name = "ClearLog";
+            this.ClearLog.Size = new System.Drawing.Size(106, 24);
+            this.ClearLog.Text = "清空";
+            this.ClearLog.Click += new System.EventHandler(this.ClearLog_Click);
+            // 
+            // UserLogger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.listBox1);
+            this.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "UserLogger";
+            this.Size = new System.Drawing.Size(365, 266);
+            this.Load += new System.EventHandler(this.UserLogger_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ClearLog;
+    }
+}
---
+++ ToolLib.Log/Logger/LoggerClass.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using System.Windows.Forms;
+
+namespace Logger
+{
+    public class LoggerClass
+    {
+        public static readonly ILog loginfo = LogManager.GetLogger("loginfo");
+        public static readonly ILog logerror = LogManager.GetLogger("logerror");
+        /// <summary>
+        /// Log队列
+        /// </summary>
+        public static Queue<LogInfo> logQueue { get; set; } = new Queue<LogInfo>() { };
+        public static void WriteLog(string info, bool ShowMsgBox = false)
+        {
+            if (loginfo.IsInfoEnabled)
+            {
+                loginfo.Info(info);
+            }
+            logQueue.Enqueue(new LogInfo{ message = info, ex = null, logLevel = MsgLevel.Info});
+            if(ShowMsgBox)
+            {
+                MessageBox.Show(info);
+            }
+        }
+        public static void WriteLog(string info, MsgLevel msgLevel, bool ShowMsgBox = false)
+        {
+            if (loginfo.IsInfoEnabled)
+            {
+                loginfo.Info(info);
+            }
+            logQueue.Enqueue(new LogInfo { message = info,ex = null, logLevel = msgLevel });
+            if (ShowMsgBox)
+            {
+                MessageBox.Show(info);
+            }
+        }
+        public static void WriteLog(string info, MsgLevel msgLevel, Exception ex, bool ShowMsgBox = false)
+        {
+            if (logerror.IsErrorEnabled)
+            {
+                logerror.Error(info, ex);
+            }
+            logQueue.Enqueue(new LogInfo { message = info, ex = ex, logLevel = msgLevel });
+            if (ShowMsgBox)
+            {
+                MessageBox.Show(info);
+            }
+        }
+        public static void WriteLog(string info, Exception ex, bool ShowMsgBox = false)
+        {
+            if (logerror.IsErrorEnabled)
+            {
+                logerror.Error(info, ex);
+            }
+            logQueue.Enqueue(new LogInfo { message = info, ex = ex, logLevel = MsgLevel.Exception });
+            if (ShowMsgBox)
+            {
+                MessageBox.Show(info);
+            }
+        }
+    }
+    public class LogInfo
+    {
+        public string message { get; set; }
+        public MsgLevel logLevel { get; set; }
+        public Exception ex { get; set; }
+    }
+    public enum MsgLevel
+    {
+        /// <summary>
+        /// 0.调试信息输出
+        /// </summary>
+        Debug = 0,
+        /// <summary>
+        /// 1.业务信息记录
+        /// </summary>
+        Info = 1,
+        /// <summary>
+        /// 2.警告提醒（捕获的业务异常）
+        /// </summary>
+        Warn = 2,
+        /// <summary>
+        /// 3.发生了异常（捕获的系统异常）
+        /// </summary>
+        Exception = 3,
+        /// <summary>
+        /// 4.发生致命异常（未被捕获的异常|捕获的业务逻辑异常）
+        /// </summary>
+        Fatal = 4
+    }
+}
